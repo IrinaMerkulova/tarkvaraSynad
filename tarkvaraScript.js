95021f2fec0af6ab301f72a27a8ca145948e619f
irina merkulova <irina@tthk.ee>
2024-03-25 14:05:06 +0200

    projekti Failid

---
+++ tarkvaraScript.js
@@ -0,0 +1,45 @@
+function randomSyna(){
+
+    const synad=[
+        'teostus',
+        'algoritm',
+        'kavand',
+        'kasutajaliides',
+        'mudel',
+        'andmebaasi skeem',
+        'standart',
+        'tsükkel',
+        'andmetöötlus',
+        'andmestruktuur',
+        'arenduskeskkond',
+        'projektihaldus tööriist',
+        'kavandamine',
+        'iteratsioon',
+        'parandus',
+        'parandamine',
+        'koskmudel',
+        'agiilne mudel',
+        'spiraalne mudel',
+        'inkrementaalne mudel',
+        'nõudmised',
+        'realiseerimine',
+        'testimine',
+        'integreerimine',
+        'kasutamine',
+        'hooldus',
+        'eelised',
+        'puudused',
+        'elutsükkel',
+        'arendamine',
+        'valideerimine'
+
+    ];
+    //random sõna - arv
+    const juhuslikSyna=Math.floor(Math.random()*synad.length);
+
+    //võtame random sõna massivist
+    const syna=synad[juhuslikSyna];
+
+    //lisame html-lehele
+    document.getElementById("random-syna").innerHTML=syna;
+}
