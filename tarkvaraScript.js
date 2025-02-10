ff3173ece398aeb095304807eb10c1cbbe033303
Waterdud <glebsotjov@gmail.com>
2025-02-10 10:00:52 +0200

    Kõikide sõnade tõlge

--- tarkvaraScript.js
+++ tarkvaraScript.js
@@ -1,40 +1,40 @@
 function randomSyna(){
 // esimese masiivi loomine
 
-    const synad=[
-        'teostus',
-        'algoritm',
-        'kavand',
-        'kasutajaliides',
-        'mudel',
-        'andmebaasi skeem',
-        'standart',
-        'tsükkel',
-        'andmetöötlus',
-        'andmestruktuur',
-        'arenduskeskkond',
-        'projektihalduse tööriist',
-        'kavandamine',
-        'iteratsioon',
-        'parandus',
-        'parandamine',
-        'koskmudel',
-        'agiilne mudel',
-        'spiraalne mudel',
-        'inkrementaalne mudel',
-        'nõudmised',
-        'realiseerimine',
-        'testimine',
-        'integreerimine',
-        'kasutamine',
-        'hooldus',
-        'eelised',
-        'puudused',
-        'elutsükkel',
-        'arendamine',
-        'valideerimine'
-
+    const synad = [
+        'teostus', // выполнение
+        'algoritm', // алгоритм
+        'kavand', // план, проект
+        'kasutajaliides', // пользовательский интерфейс
+        'mudel', // модель
+        'andmebaasi skeem', // схема базы данных
+        'standart', // стандарт
+        'tsükkel', // цикл
+        'andmetöötlus', // обработка данных
+        'andmestruktuur', // структура данных
+        'arenduskeskkond', // среда разработки
+        'projektihalduse tööriist', // инструмент управления проектами
+        'kavandamine', // проектирование, планирование
+        'iteratsioon', // итерация
+        'parandus', // исправление
+        'parandamine', // улучшение, исправление
+        'koskmudel', // каскадная модель
+        'agiilne mudel', // гибкая (Agile) модель
+        'spiraalne mudel', // спиральная модель
+        'inkrementaalne mudel', // инкрементальная модель
+        'nõudmised', // требования
+        'realiseerimine', // реализация
+        'testimine', // тестирование
+        'integreerimine', // интеграция
+        'kasutamine', // использование
+        'hooldus', // обслуживание
+        'eelised', // преимущества
+        'puudused', // недостатки
+        'elutsükkel', // жизненный цикл
+        'arendamine', // разработка
+        'valideerimine' // валидация
     ];
+
     //random sõna - arv
     const juhuslikSyna=Math.floor(Math.random()*synad.length);
 
