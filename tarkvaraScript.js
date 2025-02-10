19bde4417c3ac87dadf1fe85f04fcf08d93fd0db
RipMyMartin <martinsild.mr@gmail.com>
2025-02-10 10:04:36 +0200

    Venekeelne sonastik

--- tarkvaraScript.js
+++ tarkvaraScript.js
@@ -1,46 +1,80 @@
+const synad=[
+    'teostus',
+    'algoritm',
+    'kavand',
+    'kasutajaliides',
+    'mudel',
+    'andmebaasi skeem',
+    'standart',
+    'tsükkel',
+    'andmetöötlus',
+    'andmestruktuur',
+    'arenduskeskkond',
+    'projektihalduse tööriist',
+    'kavandamine',
+    'iteratsioon',
+    'parandus',
+    'parandamine',
+    'koskmudel',
+    'agiilne mudel',
+    'spiraalne mudel',
+    'inkrementaalne mudel',
+    'nõudmised',
+    'realiseerimine',
+    'testimine',
+    'integreerimine',
+    'kasutamine',
+    'hooldus',
+    'eelised',
+    'puudused',
+    'elutsükkel',
+    'arendamine',
+    'valideerimine'
+
+];
+
+const veneSynad = [
+    'реализация',
+    'алгоритм',
+    'проект',
+    'интерфейс пользователя',
+    'модель',
+    'схема базы данных',
+    'стандарт',
+    'цикл',
+    'обработка данных',
+    'структура данных',
+    'среда разработки',
+    'инструмент управления проектом',
+    'проектирование',
+    'итерация',
+    'исправление',
+    'исправление',
+    'водопадная модель',
+    'гибкая модель',
+    'спиральная модель',
+    'инкрементальная модель',
+    'требования',
+    'реализация',
+    'тестирование',
+    'интеграция',
+    'использование',
+    'обслуживание',
+    'преимущества',
+    'недостатки',
+    'жизненный цикл',
+    'разработка',
+    'валидация'
+];
+
+
+
 function randomSyna(){
-// esimese masiivi loomine
-
-    const synad=[
-        'teostus',
-        'algoritm',
-        'kavand',
-        'kasutajaliides',
-        'mudel',
-        'andmebaasi skeem',
-        'standart',
-        'tsükkel',
-        'andmetöötlus',
-        'andmestruktuur',
-        'arenduskeskkond',
-        'projektihalduse tööriist',
-        'kavandamine',
-        'iteratsioon',
-        'parandus',
-        'parandamine',
-        'koskmudel',
-        'agiilne mudel',
-        'spiraalne mudel',
-        'inkrementaalne mudel',
-        'nõudmised',
-        'realiseerimine',
-        'testimine',
-        'integreerimine',
-        'kasutamine',
-        'hooldus',
-        'eelised',
-        'puudused',
-        'elutsükkel',
-        'arendamine',
-        'valideerimine'
-
-    ];
-    //random sõna - arv
     const juhuslikSyna=Math.floor(Math.random()*synad.length);
-
-    //võtame random sõna massivist
     const syna=synad[juhuslikSyna];
-
-    //lisame html-lehele
     document.getElementById("random-syna").innerHTML=syna;
+}
+
+function inputSyna(){
+    
 }
