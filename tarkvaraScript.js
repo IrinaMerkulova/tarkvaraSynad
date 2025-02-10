48a951bdf933d8a07fb7d36a8f4d15637ce3ef55
Bogdan Sergachev <bsergachev@gmail.com>
2025-02-10 09:56:54 +0200

    Lisatud tõlke kontrollimine

--- tarkvaraScript.js
+++ tarkvaraScript.js
@@ -67,13 +67,33 @@ function randomSyna(){
         'жизненный цикл',
         'разработка',
         'валидация'
-    ]
+    ];
     //random sõna - arv
     const juhuslikSyna=Math.floor(Math.random()*synad.length);
 
     //võtame random sõna massivist
     const syna=synad[juhuslikSyna];
 
+    const translationIndex = synad.indexOf(syna);
+    const translatedWords = veneSynad[translationIndex];
+
     //lisame html-lehele
     document.getElementById("random-syna").innerHTML=syna;
+}
+
+
+function checkTranslation() {
+    const userInput = document.getElementById('kontroll').value;
+    const correctTranslation = document.getElementById('kontroll').getAttribute('data-translation');
+
+    const responseElement = document.getElementById('vastus');
+
+
+    if (userInput.trim().toLowerCase() === correctTranslation.toLowerCase()) {
+        responseElement.innerHTML = "Õige!";
+        responseElement.style.color = "green";
+    } else {
+        responseElement.innerHTML = "Vale!";
+        responseElement.style.color = "red";
+    }
 }
