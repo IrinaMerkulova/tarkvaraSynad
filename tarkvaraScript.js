7f043ea57067d8d535fe75aacd77367e8f83e0c9
phenibut645 <doe481157@gmail.com>
2025-02-10 10:37:33 +0200

    vene ja eesti sõnad tõlkimine on lisatud

--- tarkvaraScript.js
+++ tarkvaraScript.js
@@ -1,46 +1,76 @@
-function randomSyna(){
+const synad = {
+    'teostus': "исполнение",
+    'algoritm': "алгоритм",
+    'kavand': "набросок",
+    'kasutajaliides': "интерфейс пользователя",
+    'mudel': "модель",
+    'andmebaasi skeem': "схема базы данных",
+    'standart': "стандарт",
+    'tsükkel': "цикл",
+    'andmetöötlus': "обработка данных",
+    'andmestruktuur': "структура данных",
+    'arenduskeskkond': "среда разработки",
+    'projektihalduse tööriist': "инструмент управления проектом",
+    'kavandamine': "",
+    'iteratsioon': "итерация",
+    'parandus': "правка",
+    'parandamine': "исправление",
+    'koskmudel': "",
+    'agiilne mudel': "агильная модель",
+    'spiraalne mudel': "спиральная модель",
+    'inkrementaalne mudel': "инкриментальная модель",
+    'nõudmised': "требования",
+    'realiseerimine': "реализация",
+    'testimine': "тестирование",
+    'integreerimine': "интеграция",
+    'kasutamine': "использование",
+    'hooldus': "управление",
+    'eelised': "преимущество",
+    'puudused': "от",
+    'elutsükkel': "бесконечный цикл",
+    'arendamine': "развитие",
+    'valideerimine': "валидация"
+};
+const currentSynad = {
+    vene:{
+        currentSyna:"",
+        tolge:""
+    },
+    eesti:{
+        currentSyna:"",
+        tolge:""
+    }
+}
+
+function randomSyna(keel){
+    let arraySynad;
 // esimese masiivi loomine
-
-    const synad=[
-        'teostus',
-        'algoritm',
-        'kavand',
-        'kasutajaliides',
-        'mudel',
-        'andmebaasi skeem',
-        'standart',
-        'tsükkel',
-        'andmetöötlus',
-        'andmestruktuur',
-        'arenduskeskkond',
-        'projektihalduse tööriist',
-        'kavandamine',
-        'iteratsioon',
-        'parandus',
-        'parandamine',
-        'koskmudel',
-        'agiilne mudel',
-        'spiraalne mudel',
-        'inkrementaalne mudel',
-        'nõudmised',
-        'realiseerimine',
-        'testimine',
-        'integreerimine',
-        'kasutamine',
-        'hooldus',
-        'eelised',
-        'puudused',
-        'elutsükkel',
-        'arendamine',
-        'valideerimine'
-
-    ];
+    if(keel === "vene") arraySynad = Object.keys(synad);
+    else arraySynad = Object.values(synad);
     //random sõna - arv
-    const juhuslikSyna=Math.floor(Math.random()*synad.length);
+    const juhuslikSyna = Math.floor(Math.random() * arraySynad.length);
+    currentSynad[keel]["currentSyna"] = arraySynad[juhuslikSyna];
+    console.log(currentSynad[keel]["currentSyna"], keel);
+    if(keel === "eesti"){
+        const keys = Object.keys(synad);
 
+        console.log("eeei")
+        for(let i = 0; i < keys.length; i++){
+            if(synad[keys[i]] === currentSynad[keel]["currentSyna"]){
+                currentSynad[keel]["tolge"] = keys[i]
+            }
+        }
+    }
+    else{
+        currentSynad[keel]["tolge"] = synad[currentSynad[keel]["currentSyna"]];
+    }
     //võtame random sõna massivist
-    const syna=synad[juhuslikSyna];
 
     //lisame html-lehele
-    document.getElementById("random-syna").innerHTML=syna;
+    document.getElementById(`${keel}-syna`).innerHTML = currentSynad[keel]["currentSyna"];
+}
+function checkSyna(keel){
+    const vastusContainer = document.getElementById(`${keel}-vastus`);
+    const input = document.getElementById(`${keel}-kontroll`);
+    vastusContainer.innerText = currentSynad[keel].tolge === input.value ? "õige" : "vale";
 }
