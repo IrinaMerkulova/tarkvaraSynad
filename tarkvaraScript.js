0cb5eb001d0300c485a26e2115dd355cedc4bc1f
RipMyMartin <martinsild.mr@gmail.com>
2025-02-10 12:06:01 +0200

    words

--- tarkvaraScript.js
+++ tarkvaraScript.js
@@ -116,3 +116,21 @@ function ee_checkTranslation() {
         }
     }
 }
+function showDescription() {
+    var descriptions = {
+        "andmetöötlussüsteem": "riistvarast, tarkvarast ja inimestest koosnev süsteem andmete muutmiseks kasutatavaks informatsiooniks",
+        "draiver": "utiliitprogramm, mis teeb lisaseadmed operatsioonisüsteemile kättesaadavaks ja kasutatavaks",
+        "rakendustarkvara": "programmid, millega saab inimene arvutis midagi kasulikku teha ",
+        "BIOS": "osa süsteemist, mis on vajalik sisendi ja väljundi jaoks",
+        "systarkvara": "kõik programmid, mis tagavad, et arvuti ja tema lisaseadmed suudaksid koos funktsioneerida",
+        "Riistvara" : " (hardware) on üldisem mõiste tähistamaks igasuguseid tehnoloogilisi seadmeid (tooteid). Antud kontekstis peetakse silmas ennekõike arvuti riistvara. Viimase moodustavad kõik arvuti füüsilised komponendid: kuvar, protsessor, mälu, kettaseadmed, modem, printer, klaviatuur, hiir jms (vt moodul C). Arvutiriistvara on vajalik nii andmete sisestamisel, töötlemisel, salvestamisel kui ka töötlemise tulemuste esitamisel. ",
+        "Püsivara" : "(firmware) - talitluslikult põhimälust sõltumatul viisil püsimällu (ROM) salvestatud käsu- ja andmekogum (programm ja vastav andmestik). Püsivara on reeglina seotud mõne riistvara komponendiga ning tihti ei ole teda võimalik muuta riistvara komponenti asendamata või vähemalt ei saa seda muuta iga seadme kasutaja. BIOS (basic input/output system) on üks püsivara näiteid. BIOS on kirjutatud arvuti ROM-i ja sisaldab instruktsioone klaviatuuri sisendi ja ekraaniväljundi tarbeks. ",
+        "Operatsioonisüsteem" : "(operating system software) on tähtsaim süsteemitarkvara hulka kuuluv programm, mis juhib arvuti tööd, haldab riistvararessursse, suhtleb perifeerseadmetega ja tagab rakendusprogrammide töötamise. Kasutajatel on võimalik suhelda vahetult operatsioonisüsteemiga, kasutades selleks käsukeelt või graafilist kasutajaliidest. Operatsioonisüsteemi ülesannete hulka kuulub ressursside jaotamine erinevate rakenduste vahel, mälu ühiskasutuse juhtimine, sisend- ja väljundseadmetega suhtlemine, kasutajate haldus jne. ",
+        "Rakendustarkvara" : "(application software) on otseselt kasutaja eesmärkide täitmiseks loodud ja installeeritud tarkvara. Kõige olulisema erinevusena tulekski pidada meeles, et erinevalt süsteemitarkvarast on rakendustarkvara otseselt suunatud just kasutaja eesmärkide täitmisele.",
+        "Süsteemikonfiguratsioonifailid" :"(system configuration data) sisaldavad erinevaid andmeid, mis on vajalikud arvutisüsteemi enda toimimiseks. Sellised andmed on nii operatsioonisüsteemil kui ka rakendustarkvaral. ",
+        "Kasutajaandmed" :"(user-defined data) on spetsiifilised rakendusele, mida kasutaja jaoks arvutisüsteemi salvestatakse. "
+    };
+
+    var selectedWord = document.getElementById("words").value;
+    document.getElementById("description").innerText = descriptions[selectedWord];
+}
